082182569036293d2e12da7a2879d41a4406b64a
Jordan Sissel <jls@semicomplete.com>
2012-11-04 19:49:37 -0800

    .

--- pl2.rb
+++ pl2.rb
@@ -1,10 +1,17 @@
 $: << "lib"
 require "logstash/config/file"
+require "logstash/agent"
+require "thread"
+require "cabin"
 
 class Pipeline
   class ShutdownSignal; end
 
   def initialize(configstr)
+    @logger = Cabin::Channel.get
+    @logger.subscribe(STDOUT)
+    @logger.level = :info
+
     # hacks for now to parse a config string
     config = LogStash::Config::File.new(nil, configstr)
     agent = LogStash::Agent.new
@@ -14,12 +21,12 @@ class Pipeline
     @filters.collect(&:register)
     @outputs.collect(&:register)
 
-    @input_to_filter = SizedQueue(16)
-    @filter_to_output = SizedQueue(16)
+    @input_to_filter = SizedQueue.new(16)
+    @filter_to_output = SizedQueue.new(16)
 
     # If no filters, pipe inputs to outputs
     if @filters.empty?
-      input_to_filter = filter_to_output
+      @input_to_filter = @filter_to_output
     end
   end
 
@@ -27,22 +34,53 @@ class Pipeline
     @input_threads = @inputs.collect do |input|
       # one thread per input
       Thread.new(input) do |input|
+        Thread.current[:name] = input.class
         inputworker(input)
       end
     end
 
     # one filterworker thread, for now
-    @filter_thread = Thread.new(@filters) do |filters|
-      filterworker(filters)
+    if @filters.any?
+      @filter_thread = Thread.new(@filters, @input_to_filter, @filter_to_output) do |filters, input_queue, output_queue|
+        Thread.current[:name] = "filter"
+        filterworker(filters, input_queue, output_queue)
+      end
     end
 
     # one outputworker thread
-    @output_thread = Thread.new(@outputs) do |outputs|
-      outputworker(outputs)
+    @output_thread = Thread.new(@outputs, @filter_to_output) do |outputs, queue|
+      Thread.current[:name] = "output"
+      outputworker(outputs, queue)
     end
 
     # Now monitor input threads state
     # if all inputs are terminated, send shutdown signal to @input_to_filter
+    while true
+      if @input_threads.select(&:alive?).size == 0
+        shutdown
+        break
+      else
+        sleep 1
+      end
+    end
+  end # def run
+
+  def shutdown
+    @logger.info("Shutting down")
+    # Shutdown all inputs
+    @inputs.each(&:teardown)
+
+    # Send shutdown signal to the filters/outputs
+    @input_to_filter << ShutdownSignal
+
+    # Wait for everything to die.
+    [*@input_threads, @filter_thread, @output_thread].each do |thread|
+      next if thread.nil? # skip filter_thread if it doesn't exist.
+      while thread.alive?
+        @logger.info("Waiting for thread to finish", "thread" => thread[:name])
+        thread.join(1)
+      end
+    end
   end
 
   def inputworker(plugin)
@@ -61,77 +99,74 @@ class Pipeline
     end
   end # def 
 
-  def filterworker
-    begin
-      while true
-        event << @input_to_filter
-        break if event == ShutdownSignal
-
-        @filters.each do |filter|
-          begin
-            if filter.filter?(event)
-              success = filter.filter(event) do |newevent|
-                # if we get a new event here, it's assumed a success
-                # this will occur in filters like 'split' which turn
-                # a multiline message into multiple events.
-                filter.filter_matched(newevent) 
-                @filter_to_output << newevent
-              end
-              if success
-                # Apply any add_tag/add_field stuff if the filter was
-                # successful.
-                filter.filter_matched(event)
-              else
-                # Break filter iteration if the event was cancelled.
-                if event.cancelled?
-                  break
-                end
-              end
-            end # if filter.filter?
-          rescue => e
-            @logger.warn("An error occurred inside the #{plugin.config_name}" \
-                         "filter. LogStash has recovered from this and will " \
-                         "continue operating normally. However, this error " \
-                         "may be due to a bug, so feel free to file a ticket " \
-                         "with this full error message at " \
-                         "http://logstash.jira.com/",
-                         "plugin" => plugin.inspect, "event" => event,
-                         "exception" => e)
-          end # begin/rescue
-        end # @filters.each
-
-        # Ship the event down the pipeline
-        if !event.cancelled?
-          @filter_to_output << event
-        end
+  def filterworker(filters, input_queue, output_queue)
+    while true
+      event = input_queue.pop
+      if event == ShutdownSignal
+        # push the signal down the pipeline to the outputs.
+        output_queue << event
+        break
       end
-    rescue => e
-      @logger.error("Exception in plugin #{plugin.class}",
-                    "plugin" => plugin.inspect, "exception" => e)
+
+      filters.each do |filter|
+        begin
+          if filter.filter?(event)
+            success = filter.filter(event) do |newevent|
+              # if we get a new event here, it's assumed a success
+              # this will occur in filters like 'split' which turn
+              # a multiline message into multiple events.
+              filter.filter_matched(newevent) 
+              @filter_to_output << newevent
+            end
+            if success
+              # Apply any add_tag/add_field stuff if the filter was
+              # successful.
+              filter.filter_matched(event)
+            else
+              # Break filter iteration if the event was cancelled.
+              if event.cancelled?
+                break
+              end
+            end
+          end # if filter.filter?
+        rescue => e
+          @logger.warn("An error occurred inside the #{filter.class.config_name}" \
+                       " filter. LogStash has recovered from this and will " \
+                       "continue operating normally. However, this error " \
+                       "may be due to a bug, so feel free to file a ticket " \
+                       "with this full error message at " \
+                       "http://logstash.jira.com/",
+                       "plugin" => filter.inspect, "event" => event,
+                       "exception" => e)
+        end # begin/rescue
+      end # @filters.each
+
+      # Ship the event down the pipeline
+      if !event.cancelled?
+        output_queue << event
+      end
     end
 
-    @filters.each(&:teardown)
+    filters.each(&:teardown)
   end # def filterworker
 
-  def outputworker
-    begin
-      while true
-        event << @filter_to_output
-        break if event == :shutdown
-        @outputs.each do |output|
+  def outputworker(outputs, queue)
+    while true
+      event = queue.pop
+      break if event == ShutdownSignal
+      outputs.each do |output|
+        begin
           output.receive(event) if output.output?(event)
+        rescue => e
+          @logger.error("Exception in plugin #{output.class}",
+                        "plugin" => output.inspect, "exception" => e)
         end
       end
-    rescue => e
-      @logger.error("Exception in plugin #{plugin.class}",
-                    "plugin" => plugin.inspect, "exception" => e)
     end
-    @outputs.each(&:teardown)
+    outputs.each(&:teardown)
   end # def filterworker
 end # class Pipeline
 
-trap("INT") do
-  puts "SIGINT"; shutdown(input_threads, filter_thread, output_thread)
-  exit 1
-end
-
+start = Time.now
+Pipeline.new(ARGV[0]).run
+puts "duration: #{Time.now - start}"
